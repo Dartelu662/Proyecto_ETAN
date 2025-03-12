1104af649881ea88ab54397ddbc6c0870ec2b704
Dartelu662 <dartelu662@gmail.com>
2025-03-12 13:12:48 -0600

    calificaciones maestro

--- ProyectoPortalEtan/src/app/app.routes.ts
+++ ProyectoPortalEtan/src/app/app.routes.ts
@@ -35,65 +35,77 @@ import { CapturasCursosAdmin2Component } from './admin-2/capturas-cursos-admin-2
 import { HorasVueloAdmin3Component } from './admin-3/horas-vuelo-admin-3/horas-vuelo-admin-3.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
-    { 
-      path: 'alumno', component: AlumnoComponent, 
-      children: [
-        { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-        { path: 'inicio', component: InicioAlumnoComponent },
-        { path: 'datos-generales', component: DatosGeneralesAlumnoComponent },
-        { path: 'vuelos', component: VuelosAlumnoComponent },
-        { path: 'calificaciones', component: CalificacionesAlumnoComponent },
-        { path: 'pagos', component: PagosAlumnoComponent },
-      ]
-    },
-    { 
-      path: 'maestro', component: MaestroComponent, 
-      children: [
-        { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-        { path: 'inicio', component: InicioMaestroComponent },
-        { path: 'datos-generales', component: DatosGeneralesMaestroComponent },
-        { path: 'calificaciones', component: CalificacionesMaestroComponent },
-        { path: 'cursos', component: CursosMaestroComponent },
-      ]
-    },
-    {
-      path: 'admin-1', component: Admin1Component,
-      children: [
-        { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-        { path: 'inicio', component: InicioAdmin1Component },
-        { path: 'datos-generales', component: DatosGeneralesAdmin1Component },
-        { path: 'captura-alumnos', component: CapturasAlumnosAdmin1Component},
-        { path: 'captura-maestros', component: CapturasMaestrosAdmin1Component},
-        { path: 'captura-administrativos', component: CapturasAdministrativosAdmin1Component},
-        { path: 'captura-aviones', component: CapturasAvionesAdmin1Component},
-        { path: 'captura-cursos', component: CapturasCursosAdmin1Component},
-        { path: 'pagos', component: PagosAdmin1Component},
-        { path: 'horas-vuelo', component: HorasVueloAdmin1Component},
-      ]
-    },
-    {
-      path: 'admin-2', component: Admin2Component,
-      children: [
-        { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-        { path: 'inicio', component: InicioAdmin2Component },
-        { path: 'datos-generales', component: DatosGeneralesAdmin2Component },
-        { path: 'captura-alumnos', component: CapturasAlumnosAdmin2Component},
-        { path: 'captura-maestros', component: CapturasMaestrosAdmin2Component},
-        { path: 'captura-administrativos', component: CapturasAdministrativosAdmin2Component},
-        { path: 'captura-aviones', component: CapturasAvionesAdmin2Component},
-        { path: 'captura-cursos', component: CapturasCursosAdmin2Component},
-      ]
-    },
-    {
-      path: 'admin-3', component: Admin3Component,
-      children: [
-        { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-        { path: 'inicio', component: InicioAdmin3Component },
-        { path: 'datos-generales', component: DatosGeneralesAdmin3Component },
-        { path: 'horas-vuelo', component: HorasVueloAdmin3Component},
-      ]
-    },
-    { path: '**', redirectTo: 'login' }
-  ];;
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  
+  { path: 'login', loadComponent: () => import('./login/login.component').then(m => m.LoginComponent) },
+
+  { 
+    path: 'alumno', 
+    loadComponent: () => import('./alumno/alumno.component').then(m => m.AlumnoComponent),
+    children: [
+      { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+      { path: 'inicio', loadComponent: () => import('./alumno/inicio-alumno/inicio-alumno.component').then(m => m.InicioAlumnoComponent) },
+      { path: 'datos-generales', loadComponent: () => import('./alumno/datos-generales-alumno/datos-generales-alumno.component').then(m => m.DatosGeneralesAlumnoComponent) },
+      { path: 'vuelos', loadComponent: () => import('./alumno/vuelos-alumno/vuelos-alumno.component').then(m => m.VuelosAlumnoComponent) },
+      { path: 'calificaciones', loadComponent: () => import('./alumno/calificaciones-alumno/calificaciones-alumno.component').then(m => m.CalificacionesAlumnoComponent) },
+      { path: 'pagos', loadComponent: () => import('./alumno/pagos-alumno/pagos-alumno.component').then(m => m.PagosAlumnoComponent) },
+    ]
+  },
+
+  { 
+    path: 'maestro', 
+    loadComponent: () => import('./maestro/maestro.component').then(m => m.MaestroComponent),
+    children: [
+      { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+      { path: 'inicio', loadComponent: () => import('./maestro/inicio-maestro/inicio-maestro.component').then(m => m.InicioMaestroComponent) },
+      { path: 'datos-generales', loadComponent: () => import('./maestro/datos-generales-maestro/datos-generales-maestro.component').then(m => m.DatosGeneralesMaestroComponent) },
+      { path: 'calificaciones', loadComponent: () => import('./maestro/calificaciones-maestro/calificaciones-maestro.component').then(m => m.CalificacionesMaestroComponent) },
+      { path: 'cursos', loadComponent: () => import('./maestro/cursos-maestro/cursos-maestro.component').then(m => m.CursosMaestroComponent) },
+    ]
+  },
+
+  {
+    path: 'admin-1',
+    loadComponent: () => import('./admin-1/admin-1.component').then(m => m.Admin1Component),
+    children: [
+      { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+      { path: 'inicio', loadComponent: () => import('./admin-1/inicio-admin-1/inicio-admin-1.component').then(m => m.InicioAdmin1Component) },
+      { path: 'datos-generales', loadComponent: () => import('./admin-1/datos-generales-admin-1/datos-generales-admin-1.component').then(m => m.DatosGeneralesAdmin1Component) },
+      { path: 'captura-alumnos', loadComponent: () => import('./admin-1/capturas-alumnos-admin-1/capturas-alumnos-admin-1.component').then(m => m.CapturasAlumnosAdmin1Component) },
+      { path: 'captura-maestros', loadComponent: () => import('./admin-1/capturas-maestros-admin-1/capturas-maestros-admin-1.component').then(m => m.CapturasMaestrosAdmin1Component) },
+      { path: 'captura-administrativos', loadComponent: () => import('./admin-1/capturas-administrativos-admin-1/capturas-administrativos-admin-1.component').then(m => m.CapturasAdministrativosAdmin1Component) },
+      { path: 'captura-aviones', loadComponent: () => import('./admin-1/capturas-aviones-admin-1/capturas-aviones-admin-1.component').then(m => m.CapturasAvionesAdmin1Component) },
+      { path: 'captura-cursos', loadComponent: () => import('./admin-1/capturas-cursos-admin-1/capturas-cursos-admin-1.component').then(m => m.CapturasCursosAdmin1Component) },
+      { path: 'pagos', loadComponent: () => import('./admin-1/pagos-admin-1/pagos-admin-1.component').then(m => m.PagosAdmin1Component) },
+      { path: 'horas-vuelo', loadComponent: () => import('./admin-1/horas-vuelo-admin-1/horas-vuelo-admin-1.component').then(m => m.HorasVueloAdmin1Component) },
+    ]
+  },
+
+  {
+    path: 'admin-2',
+    loadComponent: () => import('./admin-2/admin-2.component').then(m => m.Admin2Component),
+    children: [
+      { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+      { path: 'inicio', loadComponent: () => import('./admin-2/inicio-admin-2/inicio-admin-2.component').then(m => m.InicioAdmin2Component) },
+      { path: 'datos-generales', loadComponent: () => import('./admin-2/datos-generales-admin-2/datos-generales-admin-2.component').then(m => m.DatosGeneralesAdmin2Component) },
+      { path: 'captura-alumnos', loadComponent: () => import('./admin-2/capturas-alumnos-admin-2/capturas-alumnos-admin-2.component').then(m => m.CapturasAlumnosAdmin2Component) },
+      { path: 'captura-maestros', loadComponent: () => import('./admin-2/capturas-maestros-admin-2/capturas-maestros-admin-2.component').then(m => m.CapturasMaestrosAdmin2Component) },
+      { path: 'captura-administrativos', loadComponent: () => import('./admin-2/capturas-administrativos-admin-2/capturas-administrativos-admin-2.component').then(m => m.CapturasAdministrativosAdmin2Component) },
+      { path: 'captura-aviones', loadComponent: () => import('./admin-2/capturas-aviones-admin-2/capturas-aviones-admin-2.component').then(m => m.CapturasAvionesAdmin2Component) },
+      { path: 'captura-cursos', loadComponent: () => import('./admin-2/capturas-cursos-admin-2/capturas-cursos-admin-2.component').then(m => m.CapturasCursosAdmin2Component) },
+    ]
+  },
+
+  {
+    path: 'admin-3',
+    loadComponent: () => import('./admin-3/admin-3.component').then(m => m.Admin3Component),
+    children: [
+      { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+      { path: 'inicio', loadComponent: () => import('./admin-3/inicio-admin-3/inicio-admin-3.component').then(m => m.InicioAdmin3Component) },
+      { path: 'datos-generales', loadComponent: () => import('./admin-3/datos-generales-admin-3/datos-generales-admin-3.component').then(m => m.DatosGeneralesAdmin3Component) },
+      { path: 'horas-vuelo', loadComponent: () => import('./admin-3/horas-vuelo-admin-3/horas-vuelo-admin-3.component').then(m => m.HorasVueloAdmin3Component) },
+    ]
+  },
+
+  { path: '**', redirectTo: 'login' }
+  ];
--- ProyectoPortalEtan/src/app/maestro/calificaciones-maestro/calificaciones-maestro.component.ts
+++ ProyectoPortalEtan/src/app/maestro/calificaciones-maestro/calificaciones-maestro.component.ts
@@ -1,134 +1,86 @@
-import { Component, type OnInit } from "@angular/core"
-import { CommonModule } from "@angular/common"
-import { MatStepperModule } from "@angular/material/stepper"
-import { MatButtonModule } from "@angular/material/button"
-import { MatCardModule } from "@angular/material/card"
-import { MatIconModule } from "@angular/material/icon"
-import { MatDividerModule } from "@angular/material/divider"
-import { MatBadgeModule } from "@angular/material/badge"
-import { MatListModule } from "@angular/material/list"
-import { MatTooltipModule } from "@angular/material/tooltip"
-import { MatChipsModule } from "@angular/material/chips"
-import { MatTabsModule } from "@angular/material/tabs"
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-interface Student {
-  id: number
-  name: string
-  email: string
-  attendance: number
-  grade: number
+interface Grupo {
+  materia: string;
+  planEstudio: string;
+  fechaExamen: string;
+  alumnos: Alumno[];
+  edicionHabilitada: boolean;
 }
 
-interface Group {
-  id: number
-  name: string
-  schedule: string
-  classroom: string
-  subject: string
-  students: Student[]
-}
-
-interface Teacher {
-  id: number
-  name: string
-  email: string
-  department: string
-  title: string
-  // photoUrl: string
-  specialties: string[]
-  groups: Group[]
+interface Alumno {
+  nombre: string;
+  calificacion: number | null;
+  fechaActualizacion?: string;
+  horaActualizacion?: string;
 }
 
 @Component({
-  selector: "app-calificaciones-maestro",
+  selector: 'app-calificaciones-maestro',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatStepperModule,
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
-    MatDividerModule,
-    MatBadgeModule,
-    MatListModule,
-    MatTooltipModule,
-    MatChipsModule,
-    MatTabsModule,
-  ],
-  templateUrl: "./calificaciones-maestro.component.html",
-  styleUrls: ["./calificaciones-maestro.component.scss"],
+  imports: [CommonModule, FormsModule],
+  templateUrl: './calificaciones-maestro.component.html',
+  styleUrls: ['./calificaciones-maestro.component.scss']
 })
-export class CalificacionesMaestroComponent implements OnInit {
-  teacher: Teacher = {
-    id: 1,
-    name: "Dr. Carlos Rodríguez",
-    email: "carlos.rodriguez@universidad.edu",
-    department: "Ciencias Computacionales",
-    title: "Profesor Titular",
-    // photoUrl: "https://randomuser.me/api/portraits/men/42.jpg",
-    specialties: ["Inteligencia Artificial", "Desarrollo Web", "Bases de Datos"],
-    groups: [
-      {
-        id: 101,
-        name: "Grupo A - Programación Avanzada",
-        schedule: "Lunes y Miércoles 10:00 - 12:00",
-        classroom: "Aula 305",
-        subject: "Programación Avanzada",
-        students: [
-          { id: 1001, name: "Ana García", email: "ana.garcia@universidad.edu", attendance: 95, grade: 88 },
-          { id: 1002, name: "Juan Pérez", email: "juan.perez@universidad.edu", attendance: 85, grade: 76 },
-          { id: 1003, name: "María López", email: "maria.lopez@universidad.edu", attendance: 92, grade: 91 },
-          { id: 1004, name: "Pedro Sánchez", email: "pedro.sanchez@universidad.edu", attendance: 78, grade: 65 },
-          { id: 1005, name: "Laura Martínez", email: "laura.martinez@universidad.edu", attendance: 98, grade: 94 },
-        ],
-      },
-      {
-        id: 102,
-        name: "Grupo B - Bases de Datos",
-        schedule: "Martes y Jueves 14:00 - 16:00",
-        classroom: "Laboratorio 2",
-        subject: "Bases de Datos",
-        students: [
-          { id: 2001, name: "Roberto Díaz", email: "roberto.diaz@universidad.edu", attendance: 90, grade: 82 },
-          { id: 2002, name: "Carmen Ruiz", email: "carmen.ruiz@universidad.edu", attendance: 95, grade: 89 },
-          { id: 2003, name: "José Hernández", email: "jose.hernandez@universidad.edu", attendance: 65, grade: 58 },
-          { id: 2004, name: "Sofía Torres", email: "sofia.torres@universidad.edu", attendance: 88, grade: 75 },
-          { id: 2005, name: "Miguel Flores", email: "miguel.flores@universidad.edu", attendance: 92, grade: 87 },
-          { id: 2006, name: "Elena Castro", email: "elena.castro@universidad.edu", attendance: 75, grade: 68 },
-        ],
-      },
-      {
-        id: 103,
-        name: "Grupo C - Inteligencia Artificial",
-        schedule: "Viernes 9:00 - 13:00",
-        classroom: "Aula 401",
-        subject: "Inteligencia Artificial",
-        students: [
-          { id: 3001, name: "Daniel Morales", email: "daniel.morales@universidad.edu", attendance: 97, grade: 95 },
-          { id: 3002, name: "Patricia Vargas", email: "patricia.vargas@universidad.edu", attendance: 85, grade: 79 },
-          { id: 3003, name: "Fernando Ortiz", email: "fernando.ortiz@universidad.edu", attendance: 72, grade: 62 },
-          { id: 3004, name: "Gabriela Núñez", email: "gabriela.nunez@universidad.edu", attendance: 93, grade: 88 },
-        ],
-      },
-    ],
+export class CalificacionesMaestroComponent {
+  grupos: Grupo[] = [
+    {
+      materia: 'Aerodinámica',
+      planEstudio: '2024A',
+      fechaExamen: '2025-05-10',
+      alumnos: [
+        { nombre: 'Juan Pérez', calificacion: null },
+        { nombre: 'María López', calificacion: null }
+      ],
+      edicionHabilitada: true,
+    },
+    {
+      materia: 'Navegación Aérea',
+      planEstudio: '2024A',
+      fechaExamen: '2025-05-15',
+      alumnos: [
+        { nombre: 'Carlos Sánchez', calificacion: null },
+        { nombre: 'Ana Torres', calificacion: null }
+      ],
+      edicionHabilitada: true,
+    }
+  ];
+  grupoSeleccionado: Grupo | null = null;
+  periodosPasados: string[] = ['2023A', '2023B', '2022A'];
+  periodoSeleccionado: string | null = null;
+  alumnoAnterior: Alumno[] = [{ nombre: '', calificacion: null }];
+
+  seleccionarGrupo(grupo: Grupo) {
+    this.grupoSeleccionado = grupo;
+    this.alumnoAnterior = grupo.alumnos;
   }
 
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  goToGroup(stepper: any, index: number): void {
-    stepper.selectedIndex = index
+  regresarALista() {
+    this.grupoSeleccionado = null;
   }
 
-  goToTeacherInfo(stepper: any): void {
-    stepper.selectedIndex = 0
+  actualizarCalificacion(alumno: Alumno) {
+    debugger;
+    if (alumno.calificacion === null || alumno.calificacion < 0 || alumno.calificacion > 100) {
+      if(alumno.calificacion !== null && alumno.calificacion > 100) alumno.calificacion = 100;
+      if(alumno.calificacion !== null && alumno.calificacion < 0) alumno.calificacion = 0;
+    }
+    if(alumno.calificacion){
+      const ahora = new Date();
+      alumno.fechaActualizacion = ahora.toISOString().split('T')[0];
+      alumno.horaActualizacion = ahora.toTimeString().split(' ')[0];
+    }
   }
 
-  calculateAverage(students: Student[], property: "grade" | "attendance"): number {
-    if (!students || students.length === 0) return 0
-    const sum = students.reduce((acc, student) => acc + student[property], 0)
-    return sum / students.length
+  bloquearEdicion() {
+    if(this.grupoSeleccionado){
+      this.grupoSeleccionado.edicionHabilitada = false;
+    }
   }
+
+
+
+  
 }
-
